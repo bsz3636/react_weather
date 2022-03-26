7e7438647b8159b34f554e318efbcbdc363757d9
Beata Karwowska <bsz3636@o2.pl>
2022-03-26 18:57:25 +0100

    react_weather

---
+++ src/components/PickCity/PickCity.js
@@ -0,0 +1,25 @@
+import Button from '../Button/Button';
+import TextInput from '../TextInput/TextInput';
+import styles from './PickCity.module.scss';
+import { useState } from 'react';
+
+const PickCity = ({ handleCityChange }) => {
+  const [city, setCity] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    handleCityChange(city);
+    setCity('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className={styles.pickCityForm}>
+      <label>
+        <TextInput placeholder="Enter city name...." value={city} onChange={e => setCity(e.target.value)} />
+      </label>
+      <Button>Search</Button>
+    </form>
+  );
+};
+
+export default PickCity;
---
+++ src/components/WeatherBox/WeatherBox.js
@@ -0,0 +1,41 @@
+import PickCity from '../PickCity/PickCity';
+import WeatherSummary from '../WeatherSummary/WeatherSummary';
+import Loader from '../Loader/Loader';
+import { useCallback, useState } from 'react';
+import ErrorBox from '../ErrorBox/ErrorBox';
+
+const WeatherBox = (props) => {
+  const [weather, setWeather] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const handleCityChange = useCallback((city) => {
+    setError(false);
+    setIsLoading(true);
+fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=156059ccbf5e4243712dfbc2c33b9676&units=metric`)
+  .then((res) => {
+     if (res.status === 200) {
+       return res.json().then((data) => {
+         setWeather({
+           city: data.name,
+           temp: data.main.temp,
+           icon: data.weather[0].icon,
+           description: data.weather[0].main,
+         });
+         setIsLoading(false);
+       });
+     } else {
+       setError(true);
+     }
+   });
+ }, []);
+ return (
+   <section>
+     <PickCity handleCityChange={handleCityChange} />
+     {weather && !error && <WeatherSummary {...weather} />}
+     {isLoading && !error && <Loader />}
+     {error && <ErrorBox />}
+   </section>
+ );
+};
+
+export default WeatherBox;
